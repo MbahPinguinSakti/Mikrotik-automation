eb27d4fd1fb4a27d868d161fd4f3694224d5b1f8
Edward <Edwardrafael2348@gmail.com>
2024-10-15 14:38:38 +0700

    Add:dhcp-client function, nat-masquerade function

--- mikrotik.py
+++ mikrotik.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python
 
 import paramiko
+import sys
+# Mendapatkan input dari pengguna
+host = input('Masukan IP Router: ')
+usern = input('Masukan Username: ')
+passwd = input('Masukan Password: ')
+port = 22
+
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+#connect ke mikrotik
+ssh_client.connect(host, port=port, username=usern, password=passwd)
 
 def add_ip_address():
-    # Mendapatkan input dari pengguna
-    host = input('Masukan IP Router: ')
-    usern = input('Masukan Username: ')
-    passwd = input('Masukan Password: ')
-    port = 22
-
-    ip = input('Masukan IP Address: ')
-    interface = input('Masukan nomor interface (1/2/3): ')
-
-    # Membuat koneksi SSH
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     try:
-        # Menghubungkan ke MikroTik
-        ssh_client.connect(host, port=port, username=usern, password=passwd)
-
+        ip = input('Masukan IP Address: ')
+        interface = input('Masukan nomor interface (1/2/3): ')
         # Menjalankan perintah untuk menambahkan IP Address
         command = f'/ip/address/add address={ip} interface=ether{interface}'
         ssh_client.exec_command(command)
@@ -34,22 +32,10 @@ def add_ip_address():
 
     finally:
         # Menutup koneksi SSH
-        ssh_client.close()
+        print('done')
 
 def dhcpClient():
-    # Mendapatkan input dari pengguna
-    host = input('Masukan IP Router: ')
-    usern = input('Masukan Username: ')
-    passwd = input('Masukan Password: ')
-    port = 22
-
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     try:
-        # Menghubungkan ke MikroTik
-        ssh_client.connect(host, port=port, username=usern, password=passwd)
-
         # Meminta interface yang akan digunakan
         eth = input('Mau ether berapa? (1/2/3): ')
 
@@ -63,16 +49,67 @@ def dhcpClient():
 
     except Exception as e:
         print(f"Error: {e}")
+    
+    except KeyboardInterrupt:
+        print("\nProses dihentikan oleh pengguna.")
 
     finally:
         # Menutup koneksi SSH
         ssh_client.close()
 
-# Menentukan pilihan dari pengguna
-choose = int(input("1. Menambahkan IP Address, \n2. Menambahkan DHCP Client\n: "))
-if choose == 1:
-    add_ip_address()
-elif choose == 2:
-    dhcpClient()
-else:
-    print('Invalid input')
+def dhcpserver():
+    try:
+        command = f'/ip/dhcp-server setup'
+        ssh_client.exec_command(command)
+
+        output = ssh_client.exec_command('/ip/dhcp-client/print')[1].read().decode()
+        print(output)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    except KeyboardInterrupt:
+        print("\nProses dihentikan oleh pengguna.")
+
+    finally:
+        # Menutup koneksi SSH
+        ssh_client.close()
+
+def natMasquerade():
+    try:
+        oInter = input('out interface (1/2/3)?: ')
+        command = f'/ip/firewall/nat/ add chain=srcnat out-interface={oInter} action=masquerade'
+        ssh_client.exec_command(command)
+
+        output = ssh_client.exec_command('ip/firewall/nat/pr')[1].read().decode()
+        print(output)
+    
+    except Exception as e:
+        print(f"error: {e}")
+    
+    except KeyboardInterrupt:
+        print("\nProses dihentikan oleh pengguna.")
+
+    finally:
+        ssh_client.close()
+
+def main():
+    while True:
+        choose = int(input("1. Menambahkan IP Address \n2. Menambahkan DHCP Client\n3. Nat-masquerade firewall\nfirpilih salah satu atau ketik 0 untuk mengakhiri: "))
+        if choose == 1:
+            add_ip_address()
+        elif choose == 2:
+            dhcpClient()
+        elif choose == 3:
+            natMasquerade()
+        elif choose ==4:
+            dhcpserver()
+        elif choose == 0:
+            exit()
+        else:
+            print('error')
+            exit()
+        
+
+if __name__ == "__main__":
+    main()
